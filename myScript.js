dd854af3f2602066c513b5cd0d249d50c52ffd61
sgugu <188474125+Sgugu97@users.noreply.github.com>
2025-04-16 17:36:34 +0200

    add button functions

--- myScript.js
+++ myScript.js
@@ -3,20 +3,43 @@ sizer.classList= "sizer";
 sizer.textContent = "Select Size";
 document.body.appendChild(sizer);
 
-
 const container =document.createElement("div");
 document.body.appendChild(container);
 container.classList = "container";
 
+let b = 16;
 
 
-
-for (let i=0;i<64; i++){
-    let box = document.createElement("div");
-    box.classList = "box";
-    container.appendChild(box);
-    box.addEventListener("mouseover", () => {
-        box.setAttribute("style", "background-color : blue;");
-    });
+for (let a=0;a<b; a++){
+        for(let a=0; a<b; a++){
+            let box = document.createElement("div");
+            box.classList = "box";
+            container.appendChild(box);
+            box.addEventListener("mouseover", () => {
+                box.setAttribute("style", "background-color : blue;");
+            });
+        }
 }
+
+sizer.addEventListener("click", () => {
+    for (let i=0; i<256; i++){
+        const element = document.querySelector(".box");
+    container.removeChild(element);
+    };
+
+    b=20;
+
+    for (let a=0;a<b; a++){
+        for(let a=0; a<b; a++){
+            let box = document.createElement("div");
+            box.classList = "box";
+            container.appendChild(box);
+            box.addEventListener("mouseover", () => {
+                box.setAttribute("style", "background-color : blue;");
+            });
+        }
+    }
+
+    
+})
 
